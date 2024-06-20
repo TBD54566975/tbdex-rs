eabfc1bb7c038020fa64dd84b68f3eba87cabca5
Kendall Weihe <kendallweihe@gmail.com>
2024-06-20 16:14:55 -0400

    Update bound/kt/src/test/kotlin/tbdex/sdk/HelloWorldTest.kt
    Co-authored-by: nitro-neal <5314059+nitro-neal@users.noreply.github.com>

--- bound/kt/src/test/kotlin/tbdex/sdk/HelloWorldTest.kt
+++ bound/kt/src/test/kotlin/tbdex/sdk/HelloWorldTest.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import tbdex.sdk.rust.helloWorld
 
-class LocalKeyManagerTests {
+class HelloWorldTests {
     @Test
     fun `can helloWorld`() {
         helloWorld()
