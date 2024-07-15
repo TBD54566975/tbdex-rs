885daf89f055589d20da1bc28390e1748f6a804b
nitro-neal <5314059+nitro-neal@users.noreply.github.com>
2024-07-15 13:07:15 -0700

    create kt test vector and ci (#74)
    * create kt test vector and ci
    
    * update
    
    * change to macos-latest
    
    * update bound dylib
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * spacing
    
    * revert
    
    * junit cargo
    
    * rust-test
    
    * debugging
    
    * rust test as log
    
    * cargo2junit take 2
    
    * nextest
    
    * nextest with profile
    
    * update
    
    * mac sed
    
    * add protocols to name
    
    * update order status
    
    * update

---
+++ bound/kt/src/test/kotlin/tbdex/sdk/vectors/TbdexTestVectorsProtocolTest.kt
@@ -0,0 +1,59 @@
+package tbdex.sdk.vectors
+
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import tbdex.sdk.Json
+import tbdex.sdk.messages.*
+import tbdex.sdk.resources.*
+
+class TbdexTestVectorsProtocolTest {
+
+    /**
+     * Tbdex Test Vectors Message Tests
+     */
+    @Test
+    fun parse_order() {
+        testVector("parse-order.json", ::Order) { it.toJson() }
+    }
+
+    @Test
+    fun parse_orderstatus() {
+        testVector("parse-orderstatus.json", ::OrderStatus) { it.toJson() }
+    }
+
+    @Test
+    fun parse_rfq() {
+        testVector("parse-rfq.json", { input -> Rfq(input, true) }) { it.toJson() }
+    }
+
+    @Test
+    fun parse_rfq_omit_private_data() {
+        testVector("parse-rfq-omit-private-data.json", { input -> Rfq(input, false) }) { it.toJson() }
+    }
+
+    @Test
+    fun parse_balance() {
+        testVector("parse-balance.json", ::Balance) { it.toJson() }
+    }
+
+    @Test
+    fun parse_offering() {
+        testVector("parse-offering.json", ::Offering) { it.toJson() }
+    }
+
+    @Test
+    fun parse_quote() {
+        testVector("parse-quote.json", ::Quote) { it.toJson() }
+    }
+
+    private fun <T> testVector(vectorFileName: String, objectCreation: (String) -> T, toJson: (T) -> String) {
+        val vector = TestVectors.getVector(vectorFileName)
+        assertNotNull(vector)
+
+        val input = vector!!.get("input").textValue()
+        val obj = objectCreation(input)
+
+        assertEquals(vector["output"], Json.jsonMapper.readTree(toJson(obj)))
+    }
+}
---
+++ bound/kt/src/test/kotlin/tbdex/sdk/vectors/TestVectors.kt
@@ -0,0 +1,38 @@
+package tbdex.sdk.vectors
+
+import com.fasterxml.jackson.databind.JsonNode
+import tbdex.sdk.Json
+import java.nio.file.Files
+import java.nio.file.Paths
+
+object TestVectors {
+    val vectors = readVectors()
+
+    fun readVectors(): MutableMap<String, JsonNode> {
+        val vectors = mutableMapOf<String, JsonNode>()
+        val vectorFiles = arrayOf(
+            "parse-close.json",
+            "parse-offering.json",
+            "parse-order.json",
+            "parse-orderstatus.json",
+            "parse-quote.json",
+            "parse-rfq.json",
+            "parse-rfq-omit-private-data.json",
+            "parse-balance.json"
+        )
+        
+        val basePath = "../../tbdex/hosted/test-vectors/protocol/vectors/"
+
+        for (vectorFile in vectorFiles) {
+            val filePath = Paths.get(basePath, vectorFile).normalize()
+            val vectorJson = Files.newBufferedReader(filePath).use { it.readText() }
+            vectors[vectorFile] = Json.jsonMapper.readTree(vectorJson)
+        }
+
+        return vectors
+    }
+
+    fun getVector(vectorFile: String): JsonNode? {
+        return vectors[vectorFile]
+    }
+}
