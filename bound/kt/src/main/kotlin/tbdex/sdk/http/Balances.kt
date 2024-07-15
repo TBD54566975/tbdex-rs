10374d49d5d35d5376ddc65578fb139f46b32c25
Kendall Weihe <kendallw@squareup.com>
2024-07-15 11:24:50 -0400

    Add GetExchangesResponseBody

---
+++ bound/kt/src/main/kotlin/tbdex/sdk/http/Balances.kt
@@ -0,0 +1,28 @@
+package tbdex.sdk.http
+
+import tbdex.sdk.resources.Balance
+import tbdex.sdk.rust.SystemArchitecture
+import tbdex.sdk.rust.GetBalancesResponseBody as RustCoreGetBalancesResponseBody
+
+class GetBalancesResponseBody private constructor(
+    val data: List<Balance>,
+    private val rustCoreGetBalancesResponseBody: RustCoreGetBalancesResponseBody
+) {
+    init {
+        SystemArchitecture.set() // ensure the sys arch is set for first-time loading
+    }
+
+    companion object {
+        fun fromJsonString(json: String): GetBalancesResponseBody {
+            val rustCoreGetBalancesResponseBody = RustCoreGetBalancesResponseBody.fromJsonString(json)
+            val balances = rustCoreGetBalancesResponseBody.getData().data.map {
+                Balance(it)
+            }
+            return GetBalancesResponseBody(balances, rustCoreGetBalancesResponseBody)
+        }
+    }
+
+    fun toJsonString(): String {
+        return this.rustCoreGetBalancesResponseBody.toJsonString()
+    }
+}
