d374f8eaa33b35dd84a38ab0eaad6b1ad0500d79
Neal <groessler24@gmail.com>
2024-07-24 11:06:04 -0700

    adding repo choices

--- examples/hosted-wallet-kt/wallet/build.gradle.kts
+++ examples/hosted-wallet-kt/wallet/build.gradle.kts
@@ -6,12 +6,26 @@ plugins {
 repositories {
     mavenCentral()
     mavenLocal()
+
+    // Optional: Add JitPack repository if using dependencies hosted on JitPack
+    maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("com.sparkjava:spark-core:2.9.4")
-    implementation("tbdex.sdk.core:tbdex-core-kt:1.0-SNAPSHOT")
+
+    // For local development dependency.
+    // Install the dependency locally by running `mvn install` in the `bound/kt` directory
+    // implementation("tbdex.sdk.core:tbdex-core-kt:1.0-SNAPSHOT")
+
+    // GitHub dependency
+    // Reference: https://github.com/TBD54566975/tbdex-rs/packages/2210202
+    // implementation("tbdex.sdk.core:tbdex-core-kt:0.0.1")
+
+    // JitPack dependency
+    // Reference: https://jitpack.io/#TBD54566975/tbdex-rs/
+    implementation("com.github.TBD54566975:tbdex-rs:v1.0.19")
 }
 
 java {
