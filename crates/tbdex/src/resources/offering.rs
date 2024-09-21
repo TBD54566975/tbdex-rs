ec1483fe03d8b12e00d71e50c222387fbc3b0f07
Kendall Weihe <kendallw@squareup.com>
2024-09-21 13:27:05 -0400

    Latest web5, successful full examples/ kt flow

--- crates/tbdex/src/resources/offering.rs
+++ crates/tbdex/src/resources/offering.rs
@@ -326,6 +326,7 @@ mod tests {
 #[cfg(test)]
 mod tbdex_test_vectors_protocol {
     use super::*;
+    use std::fs;
 
     #[derive(Debug, serde::Deserialize)]
     pub struct TestVector {
@@ -333,15 +334,14 @@ mod tbdex_test_vectors_protocol {
         pub output: Offering,
     }
 
-    // TODO web error?
-    // #[test]
-    // fn parse_offering() {
-    //     let path = "../../tbdex/hosted/test-vectors/protocol/vectors/parse-offering.json";
-    //     let test_vector_json: String = fs::read_to_string(path).unwrap();
+    #[test]
+    fn parse_offering() {
+        let path = "../../tbdex/hosted/test-vectors/protocol/vectors/parse-offering.json";
+        let test_vector_json: String = fs::read_to_string(path).unwrap();
 
-    //     let test_vector: TestVector = serde_json::from_str(&test_vector_json).unwrap();
-    //     let parsed_offering: Offering = Offering::from_json_string(&test_vector.input).unwrap();
+        let test_vector: TestVector = serde_json::from_str(&test_vector_json).unwrap();
+        let parsed_offering: Offering = Offering::from_json_string(&test_vector.input).unwrap();
 
-    //     assert_eq!(test_vector.output, parsed_offering);
-    // }
+        assert_eq!(test_vector.output, parsed_offering);
+    }
 }
