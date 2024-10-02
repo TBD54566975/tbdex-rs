1bbc35e6795644eeea88fddc37b48608c821890a
Neal <groessler24@gmail.com>
2024-10-02 14:49:20 -0700

    add bundle

---
+++ bound/typescript/scripts/bundle_wasm.js
@@ -0,0 +1,23 @@
+import { fileURLToPath } from 'node:url';
+import esbuild from 'esbuild';
+import path from 'node:path';
+import fs from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+esbuild.buildSync({
+  entryPoints: [`${__dirname}/../src/index.ts`],  // Entry point for your project
+  format: 'esm',          // Output as ES module
+  bundle: true,           // Bundle all dependencies
+  minify: true,           // Minify the bundle for production
+  sourcemap: true,        // Enable source map for debugging
+  outfile: `${__dirname}/../dist/index.js`,  // Output file (bundled)
+  target: 'esnext',       // Target latest ECMAScript version
+  platform: 'node',       // Target platform (Node.js)
+  external: ['node_modules/*'] // Exclude node modules from the bundle
+});
+
+// Copy WASM files (if needed)
+fs.copyFileSync(path.resolve(__dirname, '../src/wasm/generated.js'), path.resolve(__dirname, '../dist/wasm/generated.js'));
+fs.copyFileSync(path.resolve(__dirname, '../src/wasm/generated.d.ts'), path.resolve(__dirname, '../dist/wasm/generated.d.ts'));
---
+++ bound/typescript/src/index.ts
@@ -0,0 +1,9 @@
+export * from './messages';
+export * from './resources';
+export * from './wasm';
+
+export * from './bearer-did';
+export * from './errors';
+export * from './key-managers';
+export * from './portable-did';
+export * from './signers';
