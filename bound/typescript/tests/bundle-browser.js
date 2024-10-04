f95da39038e2e084ada5e502c1681baaa6b33108
Neal <groessler24@gmail.com>
2024-10-04 10:42:19 -0700

    fix tests

--- bound/typescript/tests/bundle-browser.js
+++ bound/typescript/tests/bundle-browser.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 esbuild.buildSync({
-  entryPoints : [`${__dirname}/../dist/bound/typescript/tests/**/*.test.js`],
+  entryPoints: [`${__dirname}/*.test.ts`],
   format      : 'esm',
   bundle      : true,
   sourcemap   : true,
