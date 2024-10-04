72c9a9ea83354695c1a6e3014544c1f1ad75bdb7
Neal <groessler24@gmail.com>
2024-10-04 10:18:08 -0700

    fix build

--- bound/typescript/scripts/bundle_tbdex.js
+++ bound/typescript/scripts/bundle_tbdex.js
@@ -1,18 +1,18 @@
-import { fileURLToPath } from 'node:url';
-import esbuild from 'esbuild';
-import path from 'node:path';
+// import { fileURLToPath } from 'node:url';
+// import esbuild from 'esbuild';
+// import path from 'node:path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-esbuild.buildSync({
-  entryPoints: [`${__dirname}/../src/index.ts`],
-  format: 'esm',
-  bundle: true,
-  minify: true,
-  sourcemap: true,
-  outfile: `${__dirname}/../dist/index.js`,
-  target: 'esnext',
-  platform: 'node',
-  external: ['node_modules/*']
-});
+// esbuild.buildSync({
+//   entryPoints: [`${__dirname}/../src/index.ts`],
+//   format: 'esm',
+//   bundle: true,
+//   minify: true,
+//   sourcemap: true,
+//   outfile: `${__dirname}/../dist/index.js`,
+//   target: 'esnext',
+//   platform: 'node',
+//   external: ['node_modules/*']
+// });
