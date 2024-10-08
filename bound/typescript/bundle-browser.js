1f004352729c74220d9f84d902b142ff0f8126be
Kendall Weihe <kendallw@squareup.com>
2024-10-08 10:53:51 -0400

    Add cjs build, add browser build

---
+++ bound/typescript/bundle-browser.js
@@ -0,0 +1,29 @@
+import esbuild from "esbuild";
+
+const browserConfig = {
+  entryPoints : ["./src/**/*"],
+  outdir      : "./dist/browser",
+  bundle      : true,
+  format      : "esm",
+  sourcemap   : true,
+  minify      : true,
+  platform    : "browser",
+  target      : ["chrome101", "firefox108", "safari16"],
+  define      : {
+    "global": "globalThis",
+  },
+};
+
+// esm polyfilled bundle for browser
+esbuild.build({
+  ...browserConfig,
+  // outfile: "dist/browser.mjs",
+});
+
+// iife polyfilled bundle for browser
+esbuild.build({
+  ...browserConfig,
+  format     : "iife",
+  globalName : "tbDEX",
+  // outfile    : "dist/browser.js",
+});
