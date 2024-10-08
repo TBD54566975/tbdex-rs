41f6c362d1bc1f579bbe05beb783b70016c6c603
Kendall Weihe <kendallw@squareup.com>
2024-10-08 11:16:50 -0400

    Fix tests

--- bound/typescript/tests/bundle-node.js
+++ bound/typescript/tests/bundle-node.js
@@ -2,12 +2,16 @@ import { fileURLToPath } from 'node:url'
 
 import esbuild from 'esbuild'
 import path from 'node:path'
+import fs from 'node:fs'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+fs.copyFileSync(`${__dirname}/../src/wasm/generated.d.ts`, `${__dirname}/compiled/bound/typescript/src/wasm/generated.d.ts`)
+fs.copyFileSync(`${__dirname}/../src/wasm/generated.js`, `${__dirname}/compiled/bound/typescript/src/wasm/generated.js`)
+
 esbuild.buildSync({
-  entryPoints: [`${__dirname}/../dist/bound/typescript/tests/**/*.test.js`],
+  entryPoints: [`${__dirname}/compiled/bound/typescript/tests/**/*.test.js`],
   format: 'esm',
   bundle: true,
   sourcemap: true,
